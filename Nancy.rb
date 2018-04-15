cc4513c4f602500add8ae08c0ebbe49d46c2bdf1
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-15 09:45:36 -0700

    updated Nancy with test

--- Nancy.rb
+++ Nancy.rb
@@ -84,6 +84,13 @@ module Nancy
 end
 
 include Nancy::Delegator
+get "/bare-get" do
+  "Whoa, it works!"
+end
+
+post "/" do
+  request.body.read
+end
 
 
 Rack::Handler::WEBrick.run Nancy::Application, Port: 9292
---
+++ README.md
@@ -0,0 +1,3 @@
+### README
+
+This is an introductory Sinatra exercise built following this tutorial from [Thoughbot](https://robots.thoughtbot.com/lets-build-a-sinatra).
